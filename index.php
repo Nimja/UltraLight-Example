06175b11bae8b4bf362a845f51ffe68fe96442cb
Remy <remy@emesa.nl>
2014-06-19 09:38:26 +0200

    Changed index file to be much simpler.

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
 <?php
 /**
- * Core and App path, where config.php and core.php are situated. All paths need trailing /'s
- */
-define('PATH_BASE', realpath(dirname(__FILE__)) . '/');
-define('PATH_CORE', realpath(PATH_BASE . '/../../..') . '/core4/');
-define('PATH_APP', PATH_BASE . 'application/');
-/**
- * These are the basic application specific paths.
- */
-define('PATH_ASSETS', PATH_BASE . 'assets/');
-define('PATH_CACHE', PATH_BASE . 'cache/');
-/**
  * Require the core file.
  */
-require_once PATH_CORE . 'core.php';
+require_once '../../../core4/core.php';
 /**
  * Start the application.
  */
-Core::start();
+Core::start(__DIR__);
